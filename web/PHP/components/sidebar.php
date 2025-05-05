cf5cbc69a62faa958365f7d0a0d6d3208d2612ec
josephr04 <jrosas.dev@gmail.com>
2025-05-04 22:09:20 -0500

    Move sidebar component to components folder and fix style

---
+++ web/PHP/components/sidebar.php
@@ -0,0 +1,32 @@
+<!-- sidebar.php -->
+<div class="sidebar">
+    <div class="sidebar-header">
+        <a class="sidebar-brand" href="index.php">Home</a>
+    </div>
+    
+    <ul class="sidebar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="form_tablas.php">
+                <i class="fas fa-eye"></i> Ver Empleados
+            </a>
+        </li>
+        <!--
+        <li class="nav-item">
+            <a class="nav-link" href="modificar_campos.php">
+                <i class="fas fa-edit"></i> Editar/Eliminar Empleado
+            </a>
+        </li>
+        -->
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <i class="fas fa-user-plus"></i> Agregar Empleado
+            </a>
+        </li>
+    </ul>
+
+    <div class="sidebar-footer">
+        <a href="auth/logout.php" class="btn btn-danger btn-logout">
+            <i class="fas fa-sign-out-alt"></i> Cerrar Sesión
+        </a>
+    </div>
+</div>
