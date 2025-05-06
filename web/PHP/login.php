6cae4f9e9a5dfa5ab6d330671509f51a5e853d74
josephr04 <jrosas.dev@gmail.com>
2025-05-06 08:55:48 -0500

    Fix login page style and error display

--- web/PHP/login.php
+++ web/PHP/login.php
@@ -25,25 +25,31 @@
 </head>
 <body>
   <form action="auth/procesar_login.php" method="POST">
-    <section class="vh-100 gradient-custom">
+    <section class="gradient-custom">
       <div class="container py-5 h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
           <div class="col-12 col-md-8 col-lg-6 col-xl-5">
             <div class="card bg-dark text-white" style="border-radius: 1rem;">
               <div class="card-body p-5 text-center">
 
-                <div class="mb-md-5 mt-md-4 pb-5">
+                <div class="mb-md-5 mt-md-4">
 
                   <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
                   <p class="text-white-50 mb-5">Por favor, ingrese su correo institucional y su contraseña</p>
 
                   <div data-mdb-input-init class="form-outline form-white mb-4">
-                    <input type="correo_institucional" name="correo_institucional" id="typeEmailX" class="form-control form-control-lg" placeholder="Correo"/>
+                    <input type="correo_institucional" name="correo_institucional" id="typeEmailX" class="form-control form-control-lg" placeholder="Correo" required/>
                   </div>
 
                   <div data-mdb-input-init class="form-outline form-white mb-4">
-                    <input type="password" name="contrasena" id="typePasswordX" class="form-control form-control-lg" placeholder="Contraseña"/>
+                    <input type="password" name="contrasena" id="typePasswordX" class="form-control form-control-lg" placeholder="Contraseña" required/>
                   </div>
+
+                  <?php if (isset($_GET['error'])): ?>
+                    <div class="text-danger mt-2 mb-4 px-5">
+                      <?php echo htmlspecialchars($_GET['error'], ENT_QUOTES, 'UTF-8'); ?>
+                    </div>
+                  <?php endif; ?>
 
                   <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Olvidó su contraseña?</a></p>
 
